96a217a0f99efe75db5eade7e45daafa673e8afe
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-12-16 14:43:59 +0700

    PURGE

--- go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/tamboto2000/facebook
-
-go 1.14
-
-require (
-	github.com/tamboto2000/htmltojson v1.0.0
-	github.com/tamboto2000/random v1.0.0
-	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
-)
--- raw/actor.go
+++
@@ -1,9 +0,0 @@
-package raw
-
-type Actor struct {
-	Typename       string           `json:"__typename"`
-	ProfilePicture LargerProfilePic `json:"profile_picture"`
-	ID             string           `json:"id"`
-	URL            string           `json:"url,omitempty"`
-	Name           string           `json:"name,omitempty"`
-}
