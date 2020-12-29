ae895d59f4b1c8c0fe305c9f2e995f9fa9ed299b
Franklin Collin Tamboto <tamboto2000@gmail.com>
2020-12-29 12:37:21 +0700

    Delete raw directory

--- raw/bbox.go
+++
@@ -1,9 +0,0 @@
-package raw
-
-type Bbox struct {
-	Complete       bool        `json:"complete,omitempty"`
-	Result         *Result     `json:"result,omitempty"`
-	SequenceNumber int         `json:"sequence_number,omitempty"`
-	Variables      *Variables  `json:"variables,omitempty"`
-	ExtraContext   interface{} `json:"extra_context,omitempty"`
-}
--- raw/result.go
+++
@@ -1,8 +0,0 @@
-package raw
-
-type Result struct {
-	Data       *Data       `json:"data,omitempty"`
-	Extensions *Extensions `json:"extensions,omitempty"`
-	Label      string      `json:"label,omitempty"`
-	Path       []string    `json:"path,omitempty"`
-}
